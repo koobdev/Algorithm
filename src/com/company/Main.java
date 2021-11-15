ea19e814839bc9dbae92ca535d3598be8365c391
koobondo <gupnodo21@gmail.com>
2021-11-15 18:19:33 +0900

    Chapter12 ing(O(n²))

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,136 +7,152 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        String six = "666";
-        int[] minArr = new int[N];
+        int[] arr = new int[N];
 
-        // 66612
-        // 16662
-        // 12666
         for(int i=0; i<N; i++){
-            String target = Integer.toString(i);
-            String[] arr = new String[target.length()+1];
-            int min = 0;
-            for(int j=0; j<=target.length(); j++){
-                int index = 0;
-                String num = "";
+            arr[i] = Integer.parseInt(br.readLine());
+        }
 
-                for(int k=0; k<=target.length(); k++){
-                    if(k == j){
-                        arr[k] = six;
-                    }else {
-                        String s = Character.toString(target.charAt(index));
-                        arr[k] = s;
-                        index++;
-                    }
-                }
+        pivotSort(arr, 0, arr.length-1);
 
-                for(int k=0; k<arr.length; k++){
-                    num += arr[k];
-                }
+        for(int i=0; i<N; i++){
+            System.out.println(arr[i]);
+        }
+    }
 
-                if(min == 0){
-                    min = Integer.parseInt(num);
-                }else {
-                    if(min > Integer.parseInt(num)){
-                        min = Integer.parseInt(num);
-                    }
-                }
+    private static void pivotSort(int[] arr, int low, int high){
+        if(low >= high){
+            return;
+        }
+
+        int pivot = partition(arr, low, high);
+        System.out.println("pivot : " + pivot);
+        pivotSort(arr, low, pivot-1);
+        pivotSort(arr, pivot+1, high);
+    }
+
+    // 1 4 2 0 3
+    // 1 0 2 4 3
+    // 0 1 2 4 2 -> return 1 (pivot);
+    private static int partition(int[] arr, int left, int right){
+        int low = left;
+        int high = right;
+        int pivot = arr[left];
+
+        while(low < high){
+
+            // high
+            while(arr[high] > pivot && low < high){
+                high--;
+            }
+            // low
+            while(arr[low] <= pivot && low < high){
+                low++;
             }
 
-            minArr[i] = min;
+            swap(arr, low, high);
         }
+        swap(arr, left, low);
 
-        Arrays.sort(minArr);
-        for(int i=0; i<N; i++){
-            System.out.println(minArr[i]);
-        }
+        return low;
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 }
 
 class complete {
     /*
     Baekjoon 2798
+
+    */
+
+    /*
+    Baekjoon 2750
+    // O(n²) : 선택정렬
+    /*
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-    StringTokenizer numbers = new StringTokenizer(br.readLine(), " ");
-    int N = Integer.parseInt(st.nextToken());
-    int M = Integer.parseInt(st.nextToken());
+    int N = Integer.parseInt(br.readLine());
     int[] arr = new int[N];
-    int result = 0;
 
     for(int i=0; i<N; i++){
-        arr[i] = Integer.parseInt(numbers.nextToken());
+        arr[i] = Integer.parseInt(br.readLine());
     }
 
     for(int i=0; i<N; i++){
+        int min = i;
         for(int j=i+1; j<N; j++){
-            for(int k=j+1; k<N; k++){
-                int tmp = arr[i] + arr[j] + arr[k];
-                if(tmp <= M && tmp > result){
-                    result = tmp;
-                }
+            if(arr[i] > arr[j]){
+                min = j;
+            }
+        }
+        int tmp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = tmp;
+    }
+
+    for(int i=0; i<N; i++){
+        System.out.println(arr[i]);
+    }
+    */
+
+
+    // O(n²) : 삽입정렬
+    /*
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int[] arr = new int[N];
+
+    for(int i=0; i<N; i++){
+        arr[i] = Integer.parseInt(br.readLine());
+    }
+
+    for(int i=1; i<N; i++){
+        int target = arr[i];
+        int j = i-1;
+        while(j>=0 && target<arr[j]){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = target;
+    }
+
+    for(int i=0; i<N; i++){
+        System.out.println(arr[i]);
+    }
+    */
+
+    // O(n²) : 버블정렬
+    /*
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int[] arr = new int[N];
+
+    for(int i=0; i<N; i++){
+        arr[i] = Integer.parseInt(br.readLine());
+    }
+
+    for(int i=1; i<N; i++){
+        for(int j=0; j<N-i; j++){
+            if(arr[j] > arr[j+1]){
+                int tmp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = tmp;
             }
         }
     }
 
-    System.out.println(result);
-    */
-
-    /*
-    Baekjoon 2231
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
-    int result = N;
-
     for(int i=0; i<N; i++){
-        String s = Integer.toString(i);
-        int sum = Integer.parseInt(s);
-        for(int j=0; j<s.length(); j++){
-            sum += s.charAt(j)-'0';
-        }
-
-        if(sum == N && result > i){
-            result = i;
-        }
-    }
-    System.out.println(result);
-    */
-
-    /*
-    Baekjoon 7568
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
-    int[] weight = new int[N];
-    int[] height = new int[N];
-    int[] rank = new int[N];
-
-    for(int i=0; i<N; i++){
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        weight[i] = Integer.parseInt(st.nextToken());
-        height[i] = Integer.parseInt(st.nextToken());
-    }
-
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            if(i != j && weight[i] < weight[j] && height[i] < height[j]){
-                rank[i]++;
-            }
-        }
-    }
-
-    for(int i=0; i<rank.length; i++){
-        System.out.print((rank[i]+1) + " ");
+        System.out.println(arr[i]);
     }
     */
 
-    /*
-    Baekjoon 1018 - 체스판
-
-    */
 
     /*
-    Baekjoon 1436
+    Baekjoon 2798
 
     */
 }
