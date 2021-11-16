289361301f07110a90474c006f380ea9ccf3fe52
koobondo <gupnodo21@gmail.com>
2021-11-16 19:30:52 +0900

    Chapter12 ing...(~2108)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,65 +5,15 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
 
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-
-        pivotSort(arr, 0, arr.length-1);
-
-        for(int i=0; i<N; i++){
-            System.out.println(arr[i]);
-        }
-    }
-
-    private static void pivotSort(int[] arr, int low, int high){
-        if(low >= high){
-            return;
-        }
-
-        int pivot = partition(arr, low, high);
-        System.out.println("pivot : " + pivot);
-        pivotSort(arr, low, pivot-1);
-        pivotSort(arr, pivot+1, high);
-    }
-
-    // 1 4 2 0 3
-    // 1 0 2 4 3
-    // 0 1 2 4 2 -> return 1 (pivot);
-    private static int partition(int[] arr, int left, int right){
-        int low = left;
-        int high = right;
-        int pivot = arr[left];
-
-        while(low < high){
-
-            // high
-            while(arr[high] > pivot && low < high){
-                high--;
-            }
-            // low
-            while(arr[low] <= pivot && low < high){
-                low++;
-            }
-
-            swap(arr, low, high);
-        }
-        swap(arr, left, low);
-
-        return low;
-    }
-
-    private static void swap(int[] a, int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
     }
 }
-
+//
+//    double beforeTime = System.currentTimeMillis(); //코드 실행 전에 시간 받아오기
+//
+//    double afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
+//    double secDiffTime = (afterTime - beforeTime)/1000; //두 시간에 차 계산
+//        System.out.println("시간차이(m) : "+secDiffTime);
 class complete {
     /*
     Baekjoon 2798
@@ -150,6 +100,155 @@ class complete {
     }
     */
 
+
+    /*
+    Baekjoon 2751
+
+    // 퀵정렬 구현
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.parseInt(br.readLine());
+            int[] arr = new int[N];
+
+            for(int i=0; i<N; i++){
+                arr[i] = Integer.parseInt(br.readLine());
+            }
+
+            quickSort(arr, 0, arr.length-1);
+
+            for(int i=0; i<N; i++){
+                System.out.println(arr[i]);
+            }
+        }
+
+        // 1 4 2 0 3
+        private static void quickSort(int[] arr, int start, int end){
+            if(start>end) return;
+            int left = start+1;
+            int right = end;
+            int pivot = start;
+
+            while(left <= right){
+                // left
+                while(left<=end && arr[left]<arr[pivot]){
+                    left++;
+                }
+                // right
+                while(right>start && arr[right]>arr[pivot]){
+                    right--;
+                }
+
+                if(left > right){
+                    int tmp = arr[right];
+                    arr[right] = arr[pivot];
+                    arr[pivot] = tmp;
+                }else {
+                    int tmp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = tmp;
+                }
+            }
+
+            quickSort(arr, start, right-1);
+            quickSort(arr, right+1, end);
+        }
+    }
+    */
+
+
+    /*
+    Baekjoon 2751
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int N = Integer.parseInt(br.readLine());
+    ArrayList<Integer> list = new ArrayList<>();
+
+    for(int i=0; i<N; i++){
+        list.add(Integer.parseInt(br.readLine()));
+    }
+    Collections.sort(list);
+    for(int i=0; i<N; i++){
+        sb.append(list.get(i)).append('\n');
+    }
+
+    System.out.println(sb);
+    */
+
+    /*
+    Baekjoon 10989
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int N = Integer.parseInt(br.readLine());
+    int[] arr = new int[10001];
+
+    for(int i=0; i<N; i++){
+        arr[Integer.parseInt(br.readLine())]++;
+    }
+    for(int i=0; i<arr.length; i++){
+        while(arr[i]>0){
+            sb.append(i).append('\n');
+            arr[i]--;
+        }
+    }
+    System.out.println(sb);
+    */
+
+    /*
+    Baekjoon 2108
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int N = Integer.parseInt(br.readLine());
+    double sum = 0;
+    int max = 0;
+    int index = 0;
+    int[] indexArr = new int[8001];
+    int[] resultArr = new int[N];
+    ArrayList<Integer> most = new ArrayList<>();
+
+    for(int i=0; i<N; i++){
+        int num = Integer.parseInt(br.readLine());
+        sum += num;
+        indexArr[num + 4000]++ ;
+    }
+
+    for(int i=0; i<indexArr.length; i++){
+        int cnt = indexArr[i];
+        while(cnt > 0 && index < N){
+            resultArr[index] = i-4000;
+            cnt--;
+            index++;
+        }
+        if(indexArr[i] > max){
+            most.clear();
+            max = indexArr[i];
+            most.add(i-4000);
+        }else if(indexArr[i] == max){
+            most.add(i-4000);
+        }
+    }
+
+    sb.append(Math.round(sum/N)).append('\n');
+    sb.append(resultArr[(N/2)]).append('\n');
+    if(most.size() > 1){
+        sb.append(most.get(1)).append('\n');
+    }else {
+        sb.append(most.get(0)).append('\n');
+    }
+    sb.append(resultArr[N-1] - resultArr[0]).append('\n');
+
+    System.out.println(sb);
+    */
+
+    /*
+    Baekjoon 2798
+
+    */
+
+    /*
+    Baekjoon 2798
+
+    */
 
     /*
     Baekjoon 2798
